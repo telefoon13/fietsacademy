868d21f7c332c384cfbac200c59742e859d13cc8
Mike Dhoore <mike@mikedhoore.be>
2017-09-25 15:30:14 +0200

    Hoofdstuk 3

---
+++ src/main/java/be/vdab/entities/DocentenEntity.java
@@ -0,0 +1,94 @@
+package be.vdab.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "docenten", schema = "fietsacademy", catalog = "")
+public class DocentenEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String voornaam;
+	private String familienaam;
+	private BigDecimal wedde;
+	private long rijksRegisterNr;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Basic
+	@Column(name = "voornaam")
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+
+	@Basic
+	@Column(name = "familienaam")
+	public String getFamilienaam() {
+		return familienaam;
+	}
+
+	public void setFamilienaam(String familienaam) {
+		this.familienaam = familienaam;
+	}
+
+	@Basic
+	@Column(name = "wedde")
+	public BigDecimal getWedde() {
+		return wedde;
+	}
+
+	public void setWedde(BigDecimal wedde) {
+		this.wedde = wedde;
+	}
+
+	@Basic
+	@Column(name = "rijksRegisterNr")
+	public long getRijksRegisterNr() {
+		return rijksRegisterNr;
+	}
+
+	public void setRijksRegisterNr(long rijksRegisterNr) {
+		this.rijksRegisterNr = rijksRegisterNr;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		DocentenEntity that = (DocentenEntity) o;
+
+		if (id != that.id) return false;
+		if (rijksRegisterNr != that.rijksRegisterNr) return false;
+		if (voornaam != null ? !voornaam.equals(that.voornaam) : that.voornaam != null) return false;
+		if (familienaam != null ? !familienaam.equals(that.familienaam) : that.familienaam != null) return false;
+		if (wedde != null ? !wedde.equals(that.wedde) : that.wedde != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id;
+		result = 31 * result + (voornaam != null ? voornaam.hashCode() : 0);
+		result = 31 * result + (familienaam != null ? familienaam.hashCode() : 0);
+		result = 31 * result + (wedde != null ? wedde.hashCode() : 0);
+		result = 31 * result + (int) (rijksRegisterNr ^ (rijksRegisterNr >>> 32));
+		return result;
+	}
+}
